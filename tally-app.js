d5300643227be9db5725238f5a9a6d54c4e45e10
Noxolo <wendyngcobo98@gmail.com>
2024-05-23 23:48:02 +0200

    create store file and import it to tally app

--- tally-app.js
+++ tally-app.js
@@ -1,3 +1,7 @@
+import { createStore } from "./store.js";
+
+
+
 /** 
  * @callback Action
  * @param {State}
@@ -16,51 +20,6 @@
  */
 
 
-
-// initialise factory function createStore()
-const createStore = ()=>{
-    const state= {count: 0};
-    const subscribers = [];
-    
-    const getState = () => state;    // Returns the current state
-
-    const dispatch = (action) =>{
-
-        switch(action){
-            case 'ADD':
-                state.count += 1;           // Increment the count when the action type is 'ADD'
-                break;
-            case 'SUBTRACT':               // Decrement the count when the action type is 'SUBTRACT'
-                state.count -= 1;
-                break;
-            case 'RESET':
-                state.count = 0;         // Reset the count to 0
-                break;
-            default:
-                throw new Error('Wrong selection/type');            // Log an error message           
-
-        }
-        // Notify all subscribed functions of the state change
-        notifySubscribers();
-
-    };
-
-    const subscribe =(callback) =>{      // // Function to subscribe to state changes
-        subscribers.push(callback);      //Accepts a function that gets called whenever the state changes. 
-                               
-    };
-
-    const notifySubscribers = ()=>{
-        subscribers.forEach(callback => callback(getState()));
-    };
-
-    return {
-        getState,
-        dispatch,           // return methods
-        subscribe
-    };
-
-};
 
 const store = createStore(); // create create store obj
 
