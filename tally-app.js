0b40d66b99c5a02181f8a98831ec0f9a79dd42fd
Noxolo <wendyngcobo98@gmail.com>
2024-05-23 18:27:46 +0200

    create dispatch function

--- tally-app.js
+++ tally-app.js
@@ -21,6 +21,26 @@ const createStore = ()=>{
     const state= {};
     const subscribers = [];
     
-    const getState = () => state;
-    
+    const getState = () => state;    // Returns the current state
+
+    const dispatch = (action) =>{
+
+        switch(action.type){
+            case 'ADD':
+                state.count += 1;           // Increment the count when the action type is 'ADD'
+                break;
+            case 'SUBTRACT':               // Decrement the count when the action type is 'SUBTRACT'
+                state.count -= 1;
+                break;
+            case 'RESET':
+                state.count = 0;         // Reset the count to 0
+                break;
+            default:
+                throw new Error('Wrong selection/type');            // Log an error message           
+
+        }
+
+    }
+
+
 }
