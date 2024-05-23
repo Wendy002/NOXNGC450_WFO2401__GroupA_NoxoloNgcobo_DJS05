a5231e8ba3b068f03555f1bd649995e9737de85a
Noxolo <wendyngcobo98@gmail.com>
2024-05-23 18:10:42 +0200

    initialise factory function createStore

---
+++ tally-app.js
@@ -0,0 +1,23 @@
+/** 
+ * @callback Action
+ * @param {State}
+ * @returns {State}
+*/
+
+/**
+ * @callback Update
+ * @param {Action}
+*/
+
+/**
+ * @typedef {object} Store
+ * @props {Update} update
+ */
+
+
+
+// initialise factory function createStore()
+const createStore = ()=>{
+    const state= {};
+    const subscribers = [];
+}
